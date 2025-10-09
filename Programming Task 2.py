4725709eaf1ac7d77e8d43018cb6bf82aa03bffc
GillianODuadua <gillianadina.duadua@smc.pshs.edu.ph>
2025-10-09 21:08:07 +0800

    Add files via upload

--- Programming Task 2.py
+++ Programming Task 2.py
@@ -1,1 +1,13 @@
+distance = int(input("Enter distance in kilometers:"))
+rate_per_km = int(input("Enter rate per kilometer:"))
+
+
+def calculate_delivery_fee():
+    global distance, rate_per_km
+    delivery_fee = distance * rate_per_km
+    return delivery_fee
+
+total_fee = calculate_delivery_fee()
+print("Total delivery fee: ₱", total_fee)
+
 
